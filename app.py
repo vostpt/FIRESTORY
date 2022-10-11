d16eec4ab311265eedbfa8eb651cb493a2db2556
Jorge Gomes <jorge.gomes@vost.pt>
2022-10-11 09:14:22 +0100

    Fixed boolean error

--- app.py
+++ app.py
@@ -33,9 +33,6 @@ from dash import Input, Output, dcc, html
 import dash_mantine_components as dmc
 
 
-
-
-
 # ------------------------------
 #      START DASH APP
 # ------------------------------
@@ -51,10 +48,20 @@ app = dash.Dash(__name__,title='VOST PORTUGAL',suppress_callback_exceptions=True
 app.css.config.serve_locally = False 
 app.scripts.config.serve_locally = False 
 
+# Define Arrays
+
+csv_git = [2013,2014,2015,2016,2017,2018]
+csv_fogos = [2019,2020,2021]
+
+years = [2013,2014,2015,2016,2017,2018,2019,2020,2021,2022]
+
+
 # -------------------------------------
-#     GET INITIAL DATA - ALL FOR 2022
+#     DATA INGESTION AND TREATMENT
 # -------------------------------------
 
+
+# GET DATA FOR 2022 - CALLING FOGOS.API TO GET THE LATEST VALUES
 url_bar_2022 = "https://api.fogos.pt/v2/incidents/search?after=2022-01-01&limit=1000000"
 # Get response from URL 
 response_2022 = requests.get(url_bar_2022)
@@ -65,100 +72,71 @@ df_in_2022.loc[:,'date'] = pd.to_datetime(df_in_2022['date'],format='%d-%m-%Y')
 df_in_2022['month'] = pd.DatetimeIndex(df_in_2022['date']).month
 df_in_2022 = df_in_2022.sort_values(by='district', ascending=True)
 
+print("GOT DATA FROM 2022")
+
+# GET ICNF DATA FROM GITHUB FOR 2013 to 2018
+
+for i in csv_git:
+    globals()[f"df_in_{i}"] = pd.read_csv(f'https://raw.githubusercontent.com/vostpt/ICNF_DATA/main/icnf_{i}_raw.csv')
+
+print("GOT DATA FROM 2013 TO 2018")
+
+# GET FOGOS DATA FROM LOCAL CSV FOR 2019 to 2021
+    
+for i in csv_fogos:
+    globals()[f"df_in_{i}"] = pd.read_csv(f'assets/fogos_{i}.csv')
+
+print("GOT DATA FROM 2019 TO 2021")
+
+# RENAME COLUMNS FOR ICNF DATA YEARS TO HARMONIZE WITH FOGOS.PT DATA
+for i in csv_git:
+    globals()[f"df_in_{i}"] = globals()[f"df_in_{i}"].rename(columns={"Unnamed: 0": "id","MES":"month","DISTRITO":"district","ANO":"year","CONCELHO":"concelho","AREATOTAL":"icnf.burnArea.total"})
+
+print("RENAMED COLUMNS")
+
+# CREATE TOTAL RECORDS VARIABLE FOR EACH YEAR
+
+for i in years:
+    globals()[f"total_records_{i}"] = globals()[f"df_in_{i}"]['id'].nunique()
 
 
+print("CALCULATED TOTAL RECORDS PER YEAR")
 
-# -------------------------------------
-#     GET INITIAL DATA - 2021 to 2013
-# -------------------------------------
-
-# Create Dataframes from ICNF data
-df_in_2013 = pd.read_csv('https://raw.githubusercontent.com/vostpt/ICNF_DATA/main/icnf_2013_raw.csv')
-df_in_2014 = pd.read_csv('https://raw.githubusercontent.com/vostpt/ICNF_DATA/main/icnf_2014_raw.csv')
-df_in_2015 = pd.read_csv('https://raw.githubusercontent.com/vostpt/ICNF_DATA/main/icnf_2015_raw.csv')
-df_in_2016 = pd.read_csv('https://raw.githubusercontent.com/vostpt/ICNF_DATA/main/icnf_2016_raw.csv')
-df_in_2017 = pd.read_csv('https://raw.githubusercontent.com/vostpt/ICNF_DATA/main/icnf_2017_raw.csv')
-df_in_2018 = pd.read_csv('https://raw.githubusercontent.com/vostpt/ICNF_DATA/main/icnf_2018_raw.csv')
-# Create Dataframes from Fogos.PT API converted to CSV
-df_in_2019 = pd.read_csv("assets/fogos_2019.csv")
-df_in_2020 = pd.read_csv("assets/fogos_2020.csv")
-df_in_2021 = pd.read_csv("assets/fogos_2021.csv")
-
-# rename columns to English and lowercase
-df_in_2013 = df_in_2013.rename(columns={"Unnamed: 0": "id","MES":"month","DISTRITO":"district","ANO":"year","CONCELHO":"concelho","AREATOTAL":"icnf.burnArea.total"})
-df_in_2014 = df_in_2014.rename(columns={"Unnamed: 0": "id","MES":"month","DISTRITO":"district","ANO":"year","CONCELHO":"concelho","AREATOTAL":"icnf.burnArea.total"})
-df_in_2015 = df_in_2015.rename(columns={"Unnamed: 0": "id","MES":"month","DISTRITO":"district","ANO":"year","CONCELHO":"concelho","AREATOTAL":"icnf.burnArea.total"})
-df_in_2016 = df_in_2016.rename(columns={"Unnamed: 0": "id","MES":"month","DISTRITO":"district","ANO":"year","CONCELHO":"concelho","AREATOTAL":"icnf.burnArea.total"})
-df_in_2017 = df_in_2017.rename(columns={"Unnamed: 0": "id","MES":"month","DISTRITO":"district","ANO":"year","CONCELHO":"concelho","AREATOTAL":"icnf.burnArea.total"})
-df_in_2018 = df_in_2018.rename(columns={"Unnamed: 0": "id","MES":"month","DISTRITO":"district","ANO":"year","CONCELHO":"concelho","AREATOTAL":"icnf.burnArea.total"})
-
-# GET TOTAL AMOUNT OF RECORDS FOR EACH YEAR
-
-total_records_2022 = df_in_2022['id'].nunique()
-total_records_2021 = df_in_2021['id'].nunique()
-total_records_2020 = df_in_2020['id'].nunique()
-total_records_2019 = df_in_2019['id'].nunique()
-total_records_2018 = df_in_2018['id'].nunique()
-total_records_2017 = df_in_2017['id'].nunique()
-total_records_2016 = df_in_2016['id'].nunique()
-total_records_2015 = df_in_2015['id'].nunique()
-total_records_2014 = df_in_2014['id'].nunique()
-total_records_2013 = df_in_2013['id'].nunique()
-
-# GET TOTAL BURNT AREA FOR EACH YEAR
+# CALCULATE BURNT AREA FOR EACH YEAR AND ASSIGN TO A UNIQUE VARIABLE
 
 total_burn_area_measure = " ha"
 
-df_in_2022_reset_burntarea = df_in_2022['icnf.burnArea.total'].fillna(0)
-total_burnt_area_2022_number_full = df_in_2022['icnf.burnArea.total'].sum()
-total_burnt_area_2022_number = "{:.2f}".format(total_burnt_area_2022_number_full)
-total_burnt_area_2022 = total_burnt_area_2022_number + total_burn_area_measure
+for i in years:
+    globals()[f"df_in_{i}_reset_burntarea"] = globals()[f"df_in_{i}"]['icnf.burnArea.total'].fillna(0)
+    globals()[f"total_burnt_area_{i}_number_full"] = globals()[f"df_in_{i}"]['icnf.burnArea.total'].sum()
+    globals()[f"total_burnt_area_{i}_number"] = "{:.2f}".format(globals()[f"total_burnt_area_{i}_number_full"])
+    globals()[f"total_burnt_area_{i}"] = globals()[f"total_burnt_area_{i}_number"] + total_burn_area_measure
 
-df_in_2021_reset_burntarea = df_in_2021['icnf.burnArea.total'].fillna(0)
-total_burnt_area_2021_number_full = df_in_2021['icnf.burnArea.total'].sum()
-total_burnt_area_2021_number = "{:.2f}".format(total_burnt_area_2021_number_full)
-total_burnt_area_2021 = total_burnt_area_2021_number + total_burn_area_measure
+print("CALCULATED BURNT AREA FOR EACH YEAR AND ASSIGNED TO A UNIQUE VARIABLE")
 
-df_in_2020_reset_burntarea = df_in_2020['icnf.burnArea.total'].fillna(0)
-total_burnt_area_2020_number_full = df_in_2020['icnf.burnArea.total'].sum()
-total_burnt_area_2020_number = "{:.2f}".format(total_burnt_area_2020_number_full)
-total_burnt_area_2020 = total_burnt_area_2020_number + total_burn_area_measure
 
-df_in_2019_reset_burntarea = df_in_2019['icnf.burnArea.total'].fillna(0)
-total_burnt_area_2019_number_full = df_in_2019['icnf.burnArea.total'].sum()
-total_burnt_area_2019_number = "{:.2f}".format(total_burnt_area_2019_number_full)
-total_burnt_area_2019 = total_burnt_area_2019_number + total_burn_area_measure
-
-# INITIAL SETTINGS AND VALUES
+# INITIAL DROPDOWN SETTINGS AND VALUES
 
 district_value = "Aveiro"
 county_value = "Aveiro"
 
+print("Created initial dropdown settings")
+
+for i in years:
+    print(i)
+    globals()[f"df_dropdown_county_{i}"] =  globals()[f"df_in_{i}"][globals()[f"df_in_{i}"]['district']== district_value]
+    globals()[f"df_county_records_{i}_final"] =  globals()[f"df_dropdown_county_{i}"][ globals()[f"df_dropdown_county_{i}"]['concelho']== county_value]
 
 
-df_dropdown_county_2022 = df_in_2022[df_in_2022['district']== district_value]
-df_county_records_2022_final = df_dropdown_county_2022[df_dropdown_county_2022['concelho']== county_value]
+#df_dropdown_county_2013 = df_in_2013[df_in_2013['district']== district_value]
+# df_county_records_2013_final = df_dropdown_county_2013[df_dropdown_county_2013['concelho']== county_value]
 
-df_dropdown_county_2021 = df_in_2021[df_in_2021['district'] == district_value]
-df_county_records_2021_final = df_dropdown_county_2021[df_dropdown_county_2021['concelho']== county_value]
+for i in years:
+    globals()[f"total_district_records_{i}"] = globals()[f"df_dropdown_county_{i}"]["id"].nunique()
+    globals()[f"total_county_records_{i}"] = globals()[f"df_county_records_{i}_final"]["id"].nunique()
 
-df_dropdown_county_2020 = df_in_2020[df_in_2020['district'] == district_value]
-df_county_records_2020_final = df_dropdown_county_2020[df_dropdown_county_2020['concelho']== county_value]
-
-df_dropdown_county_2019 = df_in_2019[df_in_2019['district'] == district_value]
-df_county_records_2019_final = df_dropdown_county_2019[df_dropdown_county_2019['concelho']== county_value]
-
-total_district_records_2022 = df_dropdown_county_2022['id'].nunique()
-total_county_records_2022 = df_county_records_2022_final['id'].nunique()
-
-total_district_records_2021 = df_dropdown_county_2021['id'].nunique()
-total_district_records_2020 = df_dropdown_county_2020['id'].nunique()
-total_district_records_2019 = df_dropdown_county_2019['id'].nunique()
-
-total_county_records_2021 = df_county_records_2021_final['id'].nunique()
-total_county_records_2020 = df_county_records_2020_final['id'].nunique()
-total_county_records_2019 = df_county_records_2019_final['id'].nunique()
-
+#total_district_records_2022 = df_dropdown_county_2022['id'].nunique()
+#total_county_records_2022 = df_county_records_2022_final['id'].nunique()
 
 
 
